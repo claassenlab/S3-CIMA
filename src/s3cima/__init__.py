ac3394fc9434f12b5cb1745caac017327914e14c
gabriel-dvl <gduvalkieffer@gmail.com>
2025-12-03 15:44:45 +0100

    chagne name

--- src/s3cima/__init__.py
+++ src/s3cima/__init__.py
@@ -1,5 +1,8 @@
 # according to this issue: https://github.com/fchollet/keras/issues/828
 # setting the optimizer to `fast_compile` avoids NaNs when training with ReLU
 
-import theano
-theano.config.optimizer = 'fast_compile'
+# import theano
+# theano.config.optimizer = 'fast_compile'
+
+__version__ = "0.1.0"
+
